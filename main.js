e8261b5cce13bf9d5fa4d50443bac3e2667dcb58
Spencer Sharpe <jspencersharpe@gmail.com>
2014-12-07 11:04:13 -0600

    markup complete, w/ psuedo js

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
 ;(function(){
   "use strict";
 
+  angular.module('libApp', [])
+    .controller('LibraryController', function($http){
+      var vm = this;
+    
+    vm.addNewBook = function(){
+    
+    }
+
+    })
+
 }())
+
+//addNew Task = addNewBook
+//newTask = newBook
+//todoCtrl = libCtrl
+//name = title
+//taskId = bookId
+//task = book
+//books = tasks
+//priorityOptions = readOptions
+//removeToDo = removeBook
