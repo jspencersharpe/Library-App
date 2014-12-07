5265c9a9169124e1696d8795296242e8af9641ad
Spencer Sharpe <jspencersharpe@gmail.com>
2014-12-07 11:24:35 -0600

    functionality almost complete. WIP

--- main.js
+++ main.js
@@ -5,13 +5,50 @@
     .controller('LibraryController', function($http){
       var vm = this;
     
+      $http.get('https://jsslibrary.firebaseio.com/list.json')
+      .success(function(data){
+        vm.books = data;
+      })
+      .error(function(err){
+        console.log(err);
+      })
     vm.addNewBook = function(){
-    
-    }
+      $http.post('https://jsslibrary.firebaseio.com/list.json', vm.newBook)
+        .success(function(data){
+          vm.books[data.title] = vm.newBook;
+          vm.newBook = _freshBook();
+        })
+        .error(function(err){
+          console.log(err);
+        });
+    };
 
-    })
+    vm.removeToDo = function(bookId){
+      var url = 'https://jsslibrary.firebaseio.com/' + bookId + '.json';
+      $http.delete(url)
+        .success(function(){
+          delete vm.books[bookId];
+        })
+        .error(function(err){
+          console.log(err);
+        });
+    };
 
-}())
+    vm.newBook = _freshBook();
+
+    vm.readOptions = {
+      all: 'All',
+      some: 'Some',
+      none: 'None'
+    };
+
+      function _freshBook(){
+        return {
+          read: 'none'
+        }
+      }
+    });
+}());
 
 //addNew Task = addNewBook
 //newTask = newBook
@@ -22,3 +59,4 @@
 //books = tasks
 //priorityOptions = readOptions
 //removeToDo = removeBook
+//_freshTask = _freshBook
